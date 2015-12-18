36ec7f22218c88625f5d0ec9f30fdc0efdcd2efe
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-12-18 14:54:19 +0100

    update to Activiti 5.19

--- src/test/java/net/atos/xa/activiti/util/JobRetryTest.java
+++ src/test/java/net/atos/xa/activiti/util/JobRetryTest.java
@@ -12,10 +12,10 @@ public class JobRetryTest extends PluggableActivitiTestCase {
 		JobRetry jobRetry = new JobRetry(processEngine);
 		String processInstanceId = runtimeService.startProcessInstanceByKey("JobRetry").getId();
 		assertEquals(0, FailingServiceTask.count);
-		waitForJobExecutorToProcessAllJobs(15000, 100);
+		executeJobExecutorForTime(15000, 100);
 		assertEquals(2, FailingServiceTask.count);
 		jobRetry.setRetries(processInstanceId, 1);
-		waitForJobExecutorToProcessAllJobs(8000, 100);
+		executeJobExecutorForTime(8000, 100);
 		assertEquals(3, FailingServiceTask.count);
 	}
 
